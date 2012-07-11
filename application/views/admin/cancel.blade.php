9ac1b715ff1a9cdf8a02ef6be63520fb716d8b34
Jeffrey Way <jeffrey@envato.com>
2012-07-10 21:17:06 -0400

    Clean up cancel page

--- application/views/admin/cancel.blade.php
+++ application/views/admin/cancel.blade.php
@@ -1,7 +1,7 @@
 @layout('layout')
 
 @section('content')
-	<h2>Are You Sure?</h2>
+	<h1 class="styled-heading">Are You Sure?</h1>
 	<p>Please note that, if you delete your account, all of your reminders will be deleted as well. There is no going back.</p>
 	{{ Form::open('admin/cancel', 'POST') }}
 		{{ Form::submit('Yes, Cancel My Account', ['class' => 'button']) }}
